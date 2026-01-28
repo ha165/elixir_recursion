29c86c6cfbc8e2eee8343b5e967db4be628eac06
ha165 <lumumbaharmony@gmail.com>
2026-01-28 13:30:14 +0300

    add files

--- elixir_numbers/lib/basic_math.ex
+++ elixir_numbers/lib/basic_math.ex
@@ -2,4 +2,10 @@ defmodule BasicMath do
     def add(addend_1, addend_2) do
         addend_1 + addend_2
     end
+    def substract(minuend, subtrahend) do
+        minuend - subtrahend
+    end
+    def multiply(multiplicand, multiplier) do
+        multiplicand * multiplier
+    end
 end
